3c3b977aa850ce02b28335a163b0381384fcf371
“yazanai20059-sys” <yazanai20059@iesjoanramis.org>
2026-02-18 12:50:12 +0100

    prova

---
+++ Pokemon.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string x;
+    cin >> x;
+    string y;
+    cin >> y;
+    cout << x << endl;
+}
---
+++ Prova.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <string.h>
+using namespace std;
+
+int main() { 
+string x, y;
+cout << "Escribe nom de A:" << endl;
+cin >> x;
+cout << "Escribe nom de B:" << endl;
+cin >> y;
+cout << x.insert(x.size(),y) << endl;
+return 0;
+}
