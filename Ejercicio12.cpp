7f964ddbd994d1abe60c043bc6537dfd1bc9efb7
“yazanai20059-sys” <yazanai20059@iesjoanramis.org>
2026-02-23 09:03:02 +0100

    p

--- Ejercicio12.cpp
+++ Ejercicio12.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int Sumar_llista(){
-
+int Sumar_llista(const vector <int>& l){
+    int s=0;
+    for (int v : l) {
+        s += v;
+    }
+    return s;
+}
+int Multiplicar_llista(const vector <int>& l){
+    int m=1;
+    for (int v : l) {
+        m *= v;
+    }
+    return m;
+}
+int main(){
+    vector <int> a = {1, 2, 3, 4};
+    cout << Sumar_llista(a) << endl;
+    cout << Multiplicar_llista(a) << endl;
 }
