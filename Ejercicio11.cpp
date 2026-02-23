0dafd00d0a14fdd667f6d6b9fce52168bf7b9ae4
“yazanai20059-sys” <yazanai20059@iesjoanramis.org>
2026-02-23 08:36:39 +0100

    p

--- Ejercicio11.cpp
+++ Ejercicio11.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 bool esVocal(char car) {
-    
     car = tolower(car);
-    
     if (car == 'a' || car == 'e' || car == 'i' || car == 'o' || car == 'u') {
         return true;
     }
