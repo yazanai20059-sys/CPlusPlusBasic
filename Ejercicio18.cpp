e6da49d37f73a2204664b0f59b6d71bf4b8f5d91
“yazanai20059-sys” <yazanai20059@iesjoanramis.org>
2026-02-24 09:34:47 +0100

    p

--- Ejercicio18.cpp
+++ Ejercicio18.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
-#include <list>
-
 using namespace std;
 
-void crear_punts(const list<int>& llista) {
-    for (int num : llista) {
-        for (int i = 0; i < num; i++) {
-            cout << ".";
-        }
-        cout << endl;  // Salt de línia entre elements
-    }
+void dibuixar() {
+    cout << "  (\\__/)" << endl;
+    cout << "  (o^.^)" << endl;
 }
 
 int main() {
-    list<int> numeros = {10, 8, 6, 4, 2};
-
-    crear_punts(numeros);
-
+    dibuixar(); 
     return 0;
 }
--- Ejercicio19.cpp
+++ Ejercicio19.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <limits.h>
 
-void dibuixar() {
-    cout << "  (\\__/)" << endl;
-    cout << "  (o^.^)" << endl;
+int gran_llista(const std::vector<int>& llista) {
+    int max_num = INT_MIN;
+
+    for (int num : llista) {
+        if (num > max_num) {
+            max_num = num;
+        }
+    }
+
+    return max_num;
 }
 
 int main() {
-    dibuixar(); 
+    std::vector<int> llista = {3, 45, 12, 89, 5, 23};
+
+    int mes_gran = gran_llista(llista);
+
+    std::cout << "El nombre més gran de la llista és: " << mes_gran << std::endl;
+
     return 0;
 }
